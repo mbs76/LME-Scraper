02575347d88e38292d432060ea0decf91fcfa704
María Navalón <mdnavalon@gmail.com>
2019-10-20 23:19:14 +0100

    Revert "Update scraper.py"
    This reverts commit 4ff1542cec6d261ddcd96d240e8090c2efb4403a, reversing
    changes made to f11cc6c36d0644b4588f78bf6fb10d724959eda7.

--- scraper.py
+++ scraper.py
@@ -10,17 +10,8 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-<<<<<<< HEAD
 page = requests.get("http://www.lme.com")
 soup = BeautifulSoup(page.content)
 #print(soup.prettify())
 
 
-=======
-def storeURL(str):
-    page = requests.get(str)
-    soup = BeautifulSoup(page.content)
-    print(soup.prettify())
-    
-storeURL("https://www.lme.com")
->>>>>>> 484022ac7588645fc51384c8cec429ec0aac831c
