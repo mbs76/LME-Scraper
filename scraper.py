1a242f1b6f97524f140af112b4eef996a458d7a6
Manuel Betancor <mbetancor@uoc.edu>
2019-10-20 12:21:43 +0100

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:17:05 2019
+
+@authors: mbs76 & mnavalon 
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+
