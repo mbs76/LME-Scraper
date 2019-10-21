ba754f3f0da8a95d13eea612ea9200eeb2c43f4f
Manuel Betancor <mbetancor@uoc.edu>
2019-10-21 19:10:51 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,9 +10,27 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
+
+<<<<<<< HEAD
+page = requests.get("http://www.lme.com")
+soup = BeautifulSoup(page.content)
+#print(soup.prettify())
+
+
+=======
+
 def storeURL(str):
     page = requests.get(str)
     soup = BeautifulSoup(page.content)
-    print(soup.prettify())
+    #print(soup.prettify())
+    tag = soup.table
+    head_tag = tag.thead
+    #print(head_tag.get_text())
+    body_tag = tag.tbody.tr
+   
+    for sibling  in body_tag.next_siblings:
+            print(head_tag.get_text())    
+            print(repr(sibling)) 
     
 storeURL("https://www.lme.com")
+#>>>>>>> 484022ac7588645fc51384c8cec429ec0aac831c
