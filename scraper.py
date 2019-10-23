e32bd15674eb430265c5ccf2b74bce2a56685cb5
María Navalón <mdnavalon@gmail.com>
2019-10-23 22:49:59 +0100

    Revert "Update lme.csv"
    This reverts commit 4ca1585697af494088d7b8560072030ad19d18b3.

--- scraper.py
+++ scraper.py
@@ -15,41 +15,38 @@ import os
 def storeURL(str):
     
     page = requests.get(str)
+    soup = BeautifulSoup(page.content)
     
-    if page.status_code == 200:
-        soup = BeautifulSoup(page.content)
+    tag = soup.table
+    head_tag = tag.thead
+    body_tag = tag.tbody
     
-        tag = soup.table
-        head_tag = tag.thead
-        body_tag = tag.tbody
+    separador = head_tag.get_text().find(":")
+    moneda = head_tag.get_text()[0:separador].strip()
+    fecha = head_tag.get_text()[separador + 1:].strip()
     
-        separador = head_tag.get_text().find(":")
-        moneda = head_tag.get_text()[0:separador].strip()
-        fecha = head_tag.get_text()[separador + 1:].strip()
-    
-        f = open("lme.csv", "a")
-        if os.stat('lme.csv').st_size == 0:
-            f.write("Date, Product, Currency, Value\n")
+    f = open("lme.csv", "a")
+    if os.stat('lme.csv').st_size == 0:
+        f.write("Date, Product, Currency, Value\n")
   
-        trs = body_tag.findAll('tr')
+    trs = body_tag.findAll('tr')
     
-        for tr in trs:
-            linea = ""
-            print("Fecha: " + fecha)
-            linea = fecha
-            producto = tr.findAll('th')
-            print("Producto: ", producto[0].get_text().strip())
-            linea += ", " + producto[0].get_text().strip()
-            linea += ", " + moneda
-            cantidad = tr.findAll('td')
-            print("Cantidad: ",  cantidad[0].get_text().strip())
-            linea += ', "' + cantidad[0].get_text().strip() + '"\n'
-            print(linea)
-            f.write(linea)
+    for tr in trs:
+        linea = ""
+        print("Fecha: " + fecha)
+        linea = fecha
+        producto = tr.findAll('th')
+        print("Producto: ", producto[0].get_text().strip())
+        linea += ", " + producto[0].get_text().strip()
+        linea += ", " + moneda
+        cantidad = tr.findAll('td')
+        print("Cantidad: ",  cantidad[0].get_text().strip())
+        linea += ', "' + cantidad[0].get_text().strip() + '"\n'
+        print(linea)
+        f.write(linea)
     
-        f.close()
-    else:
-        print ("Error code %s" % page.status_code)
+    f.close()
+
 
 storeURL("https://www.lme.com")
 
