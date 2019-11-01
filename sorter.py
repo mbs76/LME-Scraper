81b227a6661d044626749dd2315cb27caf07c13f
María Navalón <mdnavalon@gmail.com>
2019-11-01 23:31:47 +0000

    Create sorter.py

---
+++ sorter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:17:05 2019
+
+@authors: mbs76 & mnavalon 
+"""
+import os
+import csv
+import pandas as pd
+
+# Definimos los elementos de cada categoría de metales mediante conjuntos
+
+ferrous_metals = set(["LME Steel Rebar", "LME Steel Scrap"])
+non_ferreous_metals = set(["LME Aluminium", "LME Copper", "LME Zinc", "LME Nickel", "LME Tin", "LME Lead", "LME Aluminium Alloy", "LME NASAAC"])
+minor_metals = set(["LME Cobalt"])
+precious_metals = set(["LME Gold", "LME Silver", "LME Platinum", "LME Palladium"])
+
+
+def sorter(file_csv):
+     
+    dataset = pd.read_csv(file_csv, sep=',')
+    
+    with open("lme_sorter.csv", "w") as f:
+        writer = csv.writer(f)
+        if os.stat('lme_sorter.csv').st_size == 0:
+            writer.writerow(["Date", "Product", "Currency", "Value", "Type"])
+        for row in dataset.itertuples():
+            print(row)
+            if row.Product in ferrous_metals: 
+                tipo = "Ferrous metals"
+            elif row.Product in non_ferreous_metals: 
+                tipo = "Non ferrous metals"
+            elif row.Product in minor_metals: 
+                tipo = "Minor metals" 
+            elif row.Product in precious_metals: 
+                tipo = "Precious metals" 
+            else:
+                tipo = "NaN"
+            writer.writerow([row.Date,row.Product,row.Currency,row.Value,tipo])           
+
+
+sorter("lme.csv")
